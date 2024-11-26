7247c80c292eb696645f5a14eedd8fba009fd741
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-26 14:09:08 +0100

    first commit

---
+++ src/configs/dbConnection.js
@@ -0,0 +1,19 @@
+"use strict"
+/* -------------------------------------------------------
+    NODEJS EXPRESS | Flight API
+------------------------------------------------------- */
+// MongoDB Connection:
+
+const mongoose = require('mongoose')
+
+const dbConnection = function() {
+    mongoose.connect(process.env.MONGODB)
+        .then(() => console.log('* DB Connected * '))
+        .catch((err) => console.log('* DB Not Connected * ', err))
+}
+
+/* ------------------------------------------------------- */
+module.exports = {
+    mongoose,
+    dbConnection
+} 
