815b6cc733ba45f32f70620b94b33069da349c31
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-26 14:19:11 +0100

    "Added UserSchema to user.js model with username, password, email, and role fields, and exported the User model."

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,8 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | Flight API
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- *
 {
     "username": "test",
@@ -15,3 +15,47 @@ const { mongoose } = require('../configs/dbConnection')
 /* ------------------------------------------------------- */
 // User Model:
 
+const UserSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+      set: (password) => passwordEncrypt(password),
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: [true, "Email field must be filled."],
+      unique: [true, "This email has been taken already."],
+      validate: [
+        (email) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email),
+        "Please fill a valid email address",
+      ],
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    isStaff: {
+      type: Boolean,
+      default: false,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    collection: "users",
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", UserSchema);
