6e9b42f422e8181b7453e46e3015a17ecb1cc2eb
ucangun <149247682+ucangun@users.noreply.github.com>
2024-11-26 14:38:55 +0100

    "Added routes for users and documents in index.js"

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,15 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | Flight API
 ------------------------------------------------------- */
-const router = require('express').Router()
+const router = require("express").Router();
 /* ------------------------------------------------------- */
 
+// users:
+router.use("/users", require("./user"));
+
+// documents:
+router.use("/documents", require("./document"));
 
 /* ------------------------------------------------------- */
-module.exports = router
+module.exports = router;
